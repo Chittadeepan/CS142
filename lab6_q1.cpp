94353e597646276713611bb2e3dd69b3548f6b5e
Chittadeepan <42205302+Chittadeepan@users.noreply.github.com>
2019-03-13 17:51:17 +0000

    Add files via upload

--- lab6_q1.cpp
+++ lab6_q1.cpp
@@ -19,10 +19,10 @@ public:
       node*root;
       BiST(){
           root=NULL;
-        }
+         }
       void insert(int value){
-             insertHelper(root,value);
-        }
+           insertHelper(root,value);
+         }
       void insertHelper(node*current,int value){
            
            //Base case if root is NULL
@@ -43,7 +43,9 @@ public:
                       insertHelper(current->left,value);
                 }
              }
+             
              else{
+             //if value>current->data
              //if right is NULL ,inset there
                   if(current->right==NULL){
                       current->right=new node(value);
@@ -53,27 +55,74 @@ public:
                        insertHelper(current->right,value);
                     }
                  }
-         }    
+          }    
       void display(){
-             display2(root);
-         }
-      void display2(node*current){
-            //base condition
-             if(current==NULL){
-              
-               return ;
-            }
-            //move to left child
-             display2(current->left);
-            //display 
-             cout<<current->data<<"->";
-            //move to right child 
-             display2(current->right);
+          cout<<"Displying the numbers :\n";
+          
+	    cout<<"PRE ORDER TRAVERSAL : "<<endl;
+            displayPreorder(root);
+            
+	    cout<<"\nIN ORDER TRAVERSAL : "<<endl;
+            displayInorder(root);
+            
+	    cout<<"\nPOST ORDER TRAVERSAL : "<<endl;
+            displayPostorder(root);
           }
-       node*search(int value){
+      //inorder traversal
+      void displayInorder(node*current){
+            //base condition
+            if(current==NULL){
+              
+              return ;
+            }
+            
+            
+            //move to left child
+            displayInorder(current->left);
+            //display
+            cout<<current->data<<"->";
+            //move to right child 
+            displayInorder(current->right);
+	      
+          }
+      void displayPreorder(node*current){
+            //base condition
+            if(current==NULL){
+              
+              return ;
+            }
+            //display
+            cout<<current->data<<"->";
+            
+            //move to left child
+            displayPreorder(current->left);
+            
+            //move to right child 
+            displayPreorder(current->right);
+	      
+          }
+      void displayPostorder(node*current){
+            //base condition
+            if(current==NULL){
+              
+              return ;
+            }
+           
+            
+            //move to left child
+            displayPostorder(current->left);
+            
+            //move to right child 
+            displayPostorder(current->right);
+
+            //display
+            cout<<current->data<<"->";
+	      
+          }
+      node*search(int value){
               return search2(root,value);
           }      
-       node*search2(node*current,int value){
+      node*search2(node*current,int value){
             //if reached end of tree or value found 
               if(current==NULL || current->data==value){
                   return current;
@@ -85,31 +134,54 @@ public:
                   return search2(current->right,value);
               }
           }
-          
-            
-          
-          
-        void delet(){};
-          
-        void height(){};      
+       
+      void FancyInsert(node*root,int space){
+       
+       //base case
+               if(root==NULL){
+                   return ;
+               }
+      
+      //increase distance between levels
+               space=space+10;
+      //process right child first 
+               FancyInsert(root->right,space);
+      //print current node after space
+      //cout<<endl;
+     	    for(int i=10;i<space;i++){
+      	       cout<<" ";
+               }
+               cout<<root->data<<endl;
+        //process left child
+               FancyInsert(root->left,space);
+      
+          }
+      void FancyInsertHelper(){
+        //base case with space 0
+               FancyInsert(root,0);
+          }     
 };
 
 int main(){
       BiST b1;node*a;int b;
       
-      cout<<"Inserting numbers :";
+      cout<<"Inserting numbers : "<<endl;
       b1.insert(5);
       b1.insert(3);
       b1.insert(8);
       b1.insert(7);
       b1.insert(2);
       b1.display();
-      a=b1.search(3);
-      cout<<"\ngive the number to be searched\n";
-      cin>>b;
-      cout<<"The position of number "<<b<<" : "<<endl ;      
-      cout<<a<<endl;
       
+      
+      cout<<"\nGive the number to be searched.\n";
+      cin>>b;
+      a=b1.search(b);
+      cout<<"The position(an address of a pointer) of number "<<b<<" : "<<endl ;      
+      cout<<a<<endl;
+      cout<<"Fancy Insert :\n ";
+      b1.FancyInsertHelper();
+
       return 0;
 }
       
