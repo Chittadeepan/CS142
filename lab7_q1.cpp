b4b914a4d0a9c0286b02cdc75687029b6ec63815
Chittadeepan <42205302+Chittadeepan@users.noreply.github.com>
2019-03-25 02:53:42 +0000

    Add files via upload

--- lab7_q1.cpp
+++ lab7_q1.cpp
@@ -11,7 +11,7 @@ public:
  //ctor
  node(int value){
    left=NULL;
-   parent=NULL; 
+   parent=NULL;
    right=NULL;
    data=value;
    }
@@ -26,13 +26,13 @@ public:
            insertHelper(root,value);
          }
       void insertHelper(node*current,int value){
-           
+
            //Base case if root is NULL
-           
+
              if(root==NULL){
                root=new node(value);
-               
-             } 
+
+             }
              //else compare the curr data with value
              else if(value<(current->data)){
              //if value<curr-> data ,then
@@ -46,28 +46,29 @@ public:
                       insertHelper(current->left,value);
                 }
              }
-             
+
              else{
              //if value>current->data
-             //if right is NULL ,insert there
-                if(current->right==NULL){
+             //if right is NULL ,inset there
+                  if(current->right==NULL){
                       current->right=new node(value);
+                      current->right->parent=current;
                     }
              // else move right and call insertH
-                else{
-                      insertHelper(current->right,value);
+                  else{
+                       insertHelper(current->right,value);
                     }
                  }
-          }    
+          }
       void display(){
-            cout<<"Displying the numbers :\n";
-          
+          cout<<"Displaying the numbers :\n";
+
 	    cout<<"PRE ORDER TRAVERSAL : "<<endl;
             displayPreorder(root);
-            
+
 	    cout<<"\nIN ORDER TRAVERSAL : "<<endl;
             displayInorder(root);
-            
+
 	    cout<<"\nPOST ORDER TRAVERSAL : "<<endl;
             displayPostorder(root);
             cout<<endl;
@@ -76,60 +77,60 @@ public:
       void displayInorder(node*current){
             //base condition
             if(current==NULL){
-              
+
               return ;
             }
-            
-            
+
+
             //move to left child
             displayInorder(current->left);
             //display
             cout<<current->data<<"->";
-            //move to right child 
+            //move to right child
             displayInorder(current->right);
-	      
+
           }
       void displayPreorder(node*current){
             //base condition
             if(current==NULL){
-              
+
               return ;
             }
             //display
             cout<<current->data<<"->";
-            
+
             //move to left child
             displayPreorder(current->left);
-            
-            //move to right child 
+
+            //move to right child
             displayPreorder(current->right);
-	      
+
           }
       void displayPostorder(node*current){
             //base condition
             if(current==NULL){
-              
+
               return ;
             }
-           
-            
+
+
             //move to left child
             displayPostorder(current->left);
-            
-            //move to right child 
+
+            //move to right child
             displayPostorder(current->right);
 
             //display
             cout<<current->data<<"->";
-	      
+
           }
       node*search(int value){
               return search2(root,value);
-          }      
+          }
       node*search2(node*current,int value){
-            //if reached end of tree or value found 
+            //if reached end of tree or value found
               if(current==NULL || current->data==value){
-                  
+
                   return current;
               }
               else if(value<current->data){
@@ -139,112 +140,134 @@ public:
                   return search2(current->right,value);
               }
           }
-      //find min 
-      
+      //find min of a tree in a recursive way
+
       node*find_min(){
               find_min2(root);
-        
-          }  
-          
+
+          }
+
       node*find_min2(node*current){
              if(current->left==NULL){
-                  
+
                   return current;
-             } 
+             }
              else{
                current=current->left;
                find_min2(current);
-             }     
-        
-        
+             }
+
+
           }
-          // find max
+      // find max of a tree in a recursive way
       node*find_max(node*current){
              if(current->right==NULL){
-                  
+
                   return current;
-             } 
+             }
              else{
                current=current->right;
                find_max(current);
-             }     
-        
-        
+             }
+
+
           }
-          
+
         //replace child node with parent node
-      void replace_at_parent(node*current, node*temp){
-          
+       void replace_at_parent(node*current, node*temp){
+
              if(current->parent->left==current){
                current->parent->left=temp;
              }
              else if(current->parent->right==current){
                current->parent->right=temp;
-                 
-               
+
+
              }
 
-            
+
            }
 //swapping data
-       void value_replace(node*first, node*current){
-              node*temp;
-              temp->data=first->data;
-              first->data=current->data ;
-              current->data=temp->data ;
-           }
+       void value_replace(node*first, node*second){
+              int data=first->data;
+              first->data=second->data ;
+              second->data=data ;
+             }
           //delete node
-       void deletenode(int value){
+       void delete_node(int value){
+       //no elements
               if(search(value)==NULL){
-                
+
                 cout<<"The number "<<value<<" is not present in the tree."<<endl;
+                return;
                 }
               else{
                 node*current=search(value);
-                node*temp;            
-                 //middle node has left child     
+                node*temp;
+                 //middle node has left child
                 if(current->left!=NULL){
-                  
-                      temp=find_max(current);
                       
+                      temp=find_max(current->left);
                       value_replace(current,temp);
+                // if max of sub tree has left child,then rev link 
+                      if(temp->left!=NULL){
+                             temp->left->parent=temp->parent;
+                      }
+                             //if current-left has right child 
                       if(current->left->right!= NULL){
- 				     temp->parent->right=temp->left;
+                             temp->parent->right=temp->left;
                       }
+                      
+                             
                       else{
-                       
-                              temp->parent->left=temp->left;
+                             temp->parent->left=temp->left;
                       }
- 			
+
  		         delete temp ;
+ 		         return;
                  }
                    //middle node has right child
-		   else if(current->right!=NULL){
-			    temp=find_min2(current->right);
-			       
- 		         value_replace(current,temp);
- 			   if(current->right->left!= NULL){
- 				     temp->parent->left=temp->right;
- 			   }
- 			   else{
- 				     temp->parent->right=temp->right; 
-                      }
- 		        delete temp ;
-                 }
-				
+                else if(current->right!=NULL){
+
+                       
+                       temp=find_min2(current->right);
+                       value_replace(current,temp);
+                // if min of sub tree has right child,then rev link 
+                       if(temp->right!=NULL){
+                             temp->right->parent=temp->parent;
+                       }
+                    //if current-right has left child            
+                       if(current->right->left!= NULL){
+                             temp->parent->left=temp->right;
+                       }
+                       
+                                
+                       else{
+                       //if temp- data==current-data
+                             if(temp->data!=current->data){
+                                   temp->parent->right=temp->right;
+                             }
+                             else{
+                                    temp->parent->left=temp->right;
+                             }
+                       }
+                        delete temp ;
+                        return;
+                }
+
 		       //only root in the tree
-		  
-                            if((current==root)&&(current->left==NULL)&&(current->right==NULL)){
-                       
-                       
-                      root=NULL ;
-                 
-                      
+
+                 if((current==root)&&(current->left==NULL)&&(current->right==NULL)){
+
+
+                    root=NULL ;
+                    return;
+
                  }
                  //delete leaf node
-                else {
+                 else {
                       if(current->parent->left==current){
-                        
+
                          replace_at_parent(current, current->left);
                       }
                       else if(current->parent->right==current){
@@ -252,14 +275,18 @@ public:
                       }
                  }
              }
-         
+
          }
 
+        void deletenode(int value){
+          cout<<"Deleting "<<value<<" from the tree :"<<endl;
+          delete_node(value);
+        }
 };
 
 int main(){
       BiST b1;node*a,*c;int b,d;
-      
+
       cout<<"Inserting numbers : "<<endl;
       b1.insert(5);
       b1.insert(3);
@@ -271,20 +298,39 @@ int main(){
       b1.insert(1);
       b1.insert(6);
       b1.display();
-      
-      
-      
-     // cout<<"Fancy Insert :\n ";
-     // b1.FancyInsertHelper();
-      cout<<"Finding the address  of minimum :"<<endl ;
+
+     
+      cout<<"Finding the address  of minimum of the tree :"<<endl ;
       c=b1.find_min();
       cout<<c<<endl;
-    
-    
+      
+
       b1.deletenode(8);
+      b1.display();
+      b1.deletenode(6);
+      b1.display();
+      b1.deletenode(7);
+      b1.display();
+
+      b1.deletenode(9);
+      b1.display();
+      b1.deletenode(2);
+      b1.display();
+      b1.deletenode(1);
+      b1.display();
+   
+      b1.deletenode(3);
+      b1.display();
+     
+      b1.deletenode(4);
+      b1.display();
+
+      b1.deletenode(5);
+      
+
+      b1.display();
+      b1.deletenode(10);
       b1.display();
       
       return 0;
 }
-   
-   
